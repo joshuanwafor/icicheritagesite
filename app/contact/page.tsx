b5c185146f94e4b386e0d4bf7df645b0f4b494ea
Joshua <joshuanwafor01@gmail.com>
2025-09-15 08:15:49 -0700

    clean

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -209,7 +209,7 @@ export default function Contact() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
-                  <span className="font-pragmatica">Yingi, Rayfield – Jos South LGA, Plateau state</span>
+                  <span className="font-pragmatica">ICIC Heritage Limited, After Mac Hez eatery, Beside Kalyto pharmacy, Yingi Rayfield, Jos. Plateau State</span>
                 </div>
                 <p className="font-pragmatica text-sm text-gray-300 ml-8">
                   Call ahead to schedule a private viewing appointment
