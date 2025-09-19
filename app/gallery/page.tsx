131523ea7f55974de1ab03986d85eb2f76fc72be
Joshua <joshuanwafor01@gmail.com>
2025-09-19 06:00:28 -0700

    clean

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -196,7 +196,6 @@ export default function Gallery() {
                   allowFullScreen
                 ></iframe>
               </div>
-              <div className="absolute inset-0 bg-gradient-to-t from-black/20 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-xl"></div>
             </div>
           </div>
 
